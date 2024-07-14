4a998f7ab5f2a505730790345b5b101cbb3d5b4f
Rafael Vastag <rgvastag@gmail.com>
2024-07-14 20:09:55 -0300

    opaque tokens

--- src/main/java/com/vastag/resourceserver/core/security/config/ResourceServerConfig.java
+++ src/main/java/com/vastag/resourceserver/core/security/config/ResourceServerConfig.java
@@ -11,6 +11,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer().opaqueToken();
+        http.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer().opaqueToken()g;
     }
 }
